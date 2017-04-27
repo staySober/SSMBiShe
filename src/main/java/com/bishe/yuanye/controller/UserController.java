181582d7ce985f06b3a76f42c84dc677956b4273
sober <yuhou@yit.com>
2017-04-27 11:32:05 +0800

    add code

---
+++ src/main/java/com/bishe/yuanye/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.bishe.yuanye.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.bishe.yuanye.entity.Class;
+import com.bishe.yuanye.entity.Student;
+import com.bishe.yuanye.entity.Teacher;
+import com.bishe.yuanye.entity.User;
+import com.bishe.yuanye.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by sober on 2017/4/27.
+ *
+ * @author sober
+ * @date 2017/04/27
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+    @Autowired
+    private StudentService studentService;
+
+
+    @RequestMapping("/getStudentInfo")
+    @ResponseBody
+    public Student getStudentInfo(HttpServletRequest request){
+        User user = (User)request.getSession().getAttribute("user");
+        Student student = studentService.getStudentInfo(user.getId());
+        return student;
+    }
+
+    @RequestMapping("/getAllClass")
+    @ResponseBody
+    public List<Class> getAllClass(){
+       return studentService.getAllClass();
+    }
+
+    @RequestMapping("/getAllTeacher")
+    @ResponseBody
+    public List<Teacher> getAllTeacher(){
+       return studentService.getAllTeacher();
+    }
+}
