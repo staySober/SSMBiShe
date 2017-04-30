4274ffb20640b7e14cd03a69b6549c4005ca4682
nncy623 <yuanyedsg@126.com>
2017-04-30 11:31:00 +0800

    改变answer字段类型为text

--- src/main/java/com/bishe/yuanye/controller/QuestionController.java
+++ src/main/java/com/bishe/yuanye/controller/QuestionController.java
@@ -2,15 +2,19 @@ package com.bishe.yuanye.controller;
 
 import java.io.File;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.bishe.yuanye.common.CommonUtil;
 import com.bishe.yuanye.entity.request.QueryQuestionRequest;
 import com.bishe.yuanye.entity.response.QueryQuestionResponse;
+import com.bishe.yuanye.entity.response.SavePictureResponse;
 import com.bishe.yuanye.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
@@ -38,25 +42,29 @@ public class QuestionController {
 
     @RequestMapping(value = "/savePicture")
     @ResponseBody
-    public Model savePicture(@RequestParam("file") CommonsMultipartFile file, Model model) {
+    public SavePictureResponse savePicture(@RequestParam("file") CommonsMultipartFile file,
+        HttpServletRequest request) {
 
+        SavePictureResponse response = new SavePictureResponse();
         String fileName = file.getOriginalFilename();
         if (CommonUtil.isRightPictureFormat(fileName)) {
-            String path = imageUrl + fileName;
+            String path = request.getSession().getServletContext().getRealPath(imageUrl) + "/" + fileName;
             File newFile = new File(path);
             if (newFile.exists()) {
-                model.addAttribute("errorMsg", "文件已存在,请重命名");
+                response.errorMsg = "文件已存在,请重命名";
             } else {
                 try {
                     file.transferTo(newFile);
-                    model.addAttribute("fileName", fileName);
+                    response.fileName = fileName;
                 } catch (Exception e) {
-                    model.addAttribute("errorMsg", "文件保存失败");
+                    response.errorMsg = "文件保存失败";
                 }
             }
         } else {
-            model.addAttribute("errorMsg", "文件格式错误");
+            response.errorMsg = "文件格式错误";
         }
-        return model;
+        return response;
     }
+
+
 }
---
+++ src/main/java/com/bishe/yuanye/entity/response/SavePictureResponse.java
@@ -0,0 +1,14 @@
+package com.bishe.yuanye.entity.response;
+
+/**
+ * Created by yuanye on 2017/4/29.
+ *
+ * @author yuanye
+ * @date 2017/04/29
+ */
+public class SavePictureResponse {
+
+    public String fileName;
+
+    public String errorMsg;
+}
