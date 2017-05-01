8b4ecd5f94748a9b75732af50d58003990293c12
nncy623 <yuanyedsg@126.com>
2017-05-01 11:05:45 +0800

    a

--- src/main/java/com/bishe/yuanye/controller/QuestionController.java
+++ src/main/java/com/bishe/yuanye/controller/QuestionController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -79,9 +80,8 @@ public class QuestionController {
 
     @RequestMapping(value = "/createQuestion")
     @ResponseBody
-    public String createQuestion(HttpServletRequest request) {
+    public String createQuestion(Question question, HttpServletRequest request) {
 
-        String json = request.getParameter("question");
         request.getSession().getAttribute("user");
 
         return "";
